73d123b1132cc0db01aeb3b747e10105d6bdb3ae
Mr-Tanmoy-chakraborty <chakrabortytanmoy715@gmail.com>
2025-10-07 23:02:26 +0530

    add

--- server.js
+++ server.js
@@ -36,7 +36,4 @@ app.get("/api/mithology", (req, res) => res.json(mithologyQuestions));
 app.get("/api/politics", (req, res) => res.json(politicsQuestions));
 app.get("/api/science", (req, res) => res.json(scienceQuestions));
 
-// ✅ Export app for Vercela
-// const port=5001;
-// app.listen(port,console.log(`server is running on ${port}✅`));
 module.exports = app;
